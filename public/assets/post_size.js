b75e9290647cd79e942f9d6de8a13058423778ee
Pavel Demyanov <pavel@demjanov.ru>
2023-02-26 18:20:02 +0300

    replace big method and delete code dublicate

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,75 +1,48 @@
-const defaultDomainNames = ['com', 'de', 'net', 'uk', 'cn', 'org', 'info', 'nl', 'eu', 'ru'];
+const defaultDomainNames = ['.com', '.de', '.net', '.uk', '.cn', '.org', '.info', '.nl', '.eu', '.ru'];
 
 export default function posSize(message, domains = defaultDomainNames) {
-    message = message.toLowerCase();
-    message = clearLink(message, domains);
+    const beginLinks = ['http://', 'https://', 'www.'];
 
-    return message.length;
+    return clearLink(message.toLowerCase(), [...beginLinks, ...domains]).length;
 }
 
-function clearLink(message, domains) {
-    const howBeginLink = ['http://', 'https://', 'www.'];
+function clearLink(message, templatesForSearch) {
     const specialChars = '!@#$%^&*()+=-[]\\\';,./{}|\":<>? ';
+    let indexTemplate = 0,
+        testLink = '',
+        tempIndex = 0;
 
-    let startLink = -1;
-    let indexDomain = -1;
-    let i = 0;
-    let tempIndex = 0;
+    while(indexTemplate != templatesForSearch.length) {
+        let indexBeginLink = message.indexOf(templatesForSearch[indexTemplate], tempIndex);
 
-    while (i != howBeginLink.length - 1) {
-        startLink = message.indexOf(howBeginLink[i], tempIndex);
+        if (indexBeginLink > -1) {
+            let indexEndingLink = message.indexOf(' ', indexBeginLink) > -1 
+                    ? message.indexOf(' ', indexBeginLink) 
+                    : message.length;  
 
-        if (startLink > -1) { 
-            let endLink = message.indexOf(' ', startLink) > -1 
-                ? message.indexOf(' ', startLink) 
-                : message.length;
-    
-            let tempLink = message.slice(startLink, endLink);
-            const indexDot = tempLink.indexOf('.');
-    
+            if (templatesForSearch[indexTemplate][0] == '.') {
+                indexBeginLink = message.lastIndexOf(' ', indexBeginLink) > -1 
+                    ? message.lastIndexOf(' ', indexBeginLink) + 1 
+                    : 0;
+            }
+
+            let indexDot = (testLink = message.slice(indexBeginLink, indexEndingLink)).indexOf('.');
+
             if (indexDot > -1 
-                && indexDot != tempLink.length
-                && specialChars.indexOf(tempLink[indexDot - 1]) == -1
-                && specialChars.indexOf(tempLink[indexDot + 1]) == -1) {
-                message = message.replace(tempLink, '');
-            } else {
-                tempIndex = endLink;
-            }
+                && indexDot != 0
+                && indexDot != testLink.length
+                && specialChars.indexOf(testLink[indexDot - 1]) == -1
+                && specialChars.indexOf(testLink[indexDot + 1]) == -1) {
+                    tempIndex = indexEndingLink;
+                    message = message.replace(testLink, '');
+                } else {
+                    tempIndex = indexEndingLink;
+                }
         } else {
+            indexTemplate++;
             tempIndex = 0;
-            i++;
-        }
-    }
-
-    i = 0;
-    tempIndex = 0;
-    while (i != domains.length - 1) {
-        indexDomain = message.indexOf(`.${domains[i]}`, tempIndex); 
-        if (indexDomain > -1) {
-            let startLink = message.lastIndexOf(' ', indexDomain) > -1 
-                ? message.lastIndexOf(' ', indexDomain) + 1 
-                : 0;
-
-            let endLink = message.indexOf(' ', indexDomain) > -1 
-                ? message.indexOf(' ', indexDomain) 
-                : message.length;
-
-            let tempLink = message.slice(startLink, endLink);
-
-            if (indexDomain != 0
-                && specialChars.indexOf(message[indexDomain - 1]) == -1) {
-                tempIndex = endLink;
-                message = message.replace(tempLink, '');
-            } else {
-                tempIndex = endLink;
-            }
-        } else {
-            tempIndex = 0;
-            i++;
         }
     }
 
     return message;
 }
-
-posSize('Привет! https://github.com');
